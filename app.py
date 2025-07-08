e32b5dd9d3f64e7b78e15f6d4c7e0a897526aa9c
StickyPicky-Laptop <spickworth1991@gmail.com>
2025-07-07 22:43:36 -0400

    Good Progress

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import re
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import pandas as pd
@@ -472,4 +473,6 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
